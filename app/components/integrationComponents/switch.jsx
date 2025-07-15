d62db10cd84ed8f86500af183109780d37c682f0
mcMineyC <jedi.master@falconhosting.net>
2025-07-15 15:22:27 -0700

    keep working on components

--- app/components/integrationComponents/switch.jsx
+++ app/components/integrationComponents/switch.jsx
@@ -3,18 +3,19 @@ import { useSelector } from 'react-redux';
 import { PrimaryButton } from '../button.jsx';
 
 export const Switch = ({ integrationId }) => {
-  const integration = useSelector((state) =>
-    Object.entries(state.state).find(([key, int]) => int.id === integrationId)
-  );
+  console.log("Switch component for integrationId:", integrationId);
+  const state = useSelector((state) => state.state.value);
+  console.log(state);
+  const integration = Object.entries(state).find(([key, int]) => key === integrationId);
 
   if (!integration) {
-    return <div>
+    return <div className='integration-card'>
         <PrimaryButton color="white" className="square">Integration not found</PrimaryButton>
     </div>;
   }
 
   return (
-    <div>
+    <div className='integration-card'>
       <h2>{integration.name}</h2>
       <p>{integration.description}</p>
     </div>
